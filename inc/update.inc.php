0fabae9da65164be74af88010c33d11577570d05
unknown <VASILE.MIGEA@medlpsclu01.intranet.romgaz.ro>
2013-05-20 17:03:47 +0300

    Inserted the atribute page. Modify the database.

--- inc/update.inc.php
+++ inc/update.inc.php
@@ -1,17 +1,24 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD']=='POST'
-&& $_POST['submit']=='Save Entry')
-{	//sunt pe metoda de Submit Save
+	&& $_POST['submit']=='Save Entry'
+	&& !empty($_POST['page'])
+	&& !empty($_POST['title'])
+	&& !empty($_POST['entry']))
+{	
+	//sunt pe metoda de Submit Save
 	// Include database credentials and connect to the database
 	include_once 'db.inc.php';
 	$db = new PDO(DB_INFO, DB_USER, DB_PASS);
 		
 	// Save the entry into the database
-	$sql = "INSERT INTO entries (title, entry) VALUES (?, ?)";
+	$sql = "INSERT INTO entries (page, title, entry) VALUES (?, ?, ?)";
 	$stmt = $db->prepare($sql);
-	$stmt->execute(array($_POST['title'], $_POST['entry']));
+	$stmt->execute(array($_POST['page'],$_POST['title'],$_POST['entry']));
 	$stmt->closeCursor();
+	
+	// Sanitize the page information for use in the success URL
+	$page = htmlentities(strip_tags($_POST['page']));
 	
 	// Get the ID of the entry we just saved
 	$id_obj = $db->query("SELECT LAST_INSERT_ID()");
@@ -19,7 +26,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'
 	$id_obj->closeCursor();
 	
 	// Send the user to the new entry
-	header('Location: ../?id='.$id[0]);
+	header('Location: /blog_alin/?page='.$page.'&id='.$id[0]);
 	exit;
 }
 // If both conditions aren't met, sends the user back to the main page
