1f66801a773da5326b366994342b80e656acfa5b
unknown <VASILE.MIGEA@medlpsclu01.intranet.romgaz.ro>
2013-05-20 17:02:09 +0300

    Modify index.php. It can now know what page is blog or about.

--- index.php
+++ index.php
@@ -8,11 +8,24 @@
 	// Open a database connection
 	$db = new PDO(DB_INFO, DB_USER, DB_PASS);
 	
+	/*
+	 * Figure out what page is being requested (default is blog)
+	* Perform basic sanitization on the variable as well
+	*/
+	if(isset($_GET['page']))
+	{
+		$page = htmlentities(strip_tags($_GET['page']));
+	}
+	else
+	{
+		$page = 'blog';
+	}
+	
 	// Determine if an entry ID was passed in the URL
 	$id = (isset($_GET['id'])) ? (int) $_GET['id'] : NULL;
 	
 	// Load the entries
-	$e = retrieveEntries($db, $id);
+	$e = retrieveEntries($db, $page, $id);
 	
 	// Get the fulldisp flag and remove it from the array
 	$fulldisp = array_pop($e);
@@ -38,19 +51,21 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	<div id="entries">
 	
 		<?php
-			// If the full display flag is set, show the entry
+			// If the full display flag is set, show the entry, nu avem nicio inregistrare sau avem doar una cu id dat
 			if($fulldisp==1)
 			{
 		?>
 				<h2> <?php echo $e['title'] ?> </h2>
 				<p> <?php echo $e['entry'] ?> </p>
+				<?php if($page=='blog'): ?>
 				<p class="backlink">
-					<a href="./">Back to Latest Entries</a>
+					<a href="./">Back to Latest Entries</a>			<!-- sa vad ce inseamna "./" ii diferit de ../ -->
 				</p>
+				<?php endif; ?>
 		<?php
 			} // End the if statement
 			
-			// If the full display flag is 0, format linked entry titles
+			// If the full display flag is 0, format linked entry titles, toate titlurile
 			else
 			{
 				// Loop through each entry
@@ -67,7 +82,9 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 		?>
 		
 		<p class="backlink">
-			<a href="admin.php">Post a New Entry</a>
+			<a href="/blog_alin/admin.php?page=<?php echo $page ?>">
+				Post a New Entry
+			</a>
 		</p>
 	</div>
 </body>
